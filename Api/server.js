8cfc3459f69c4551e8c179d6f3a0df0f702283b8
FZhuu <felipezhangzhu@gmail.com>
2025-04-02 20:40:34 -0300

    Inicio

---
+++ Api/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+const usuarios = [];
+
+app.get('/api/usuario', function (req, res) {
+    res.json(usuarios);
+});
+
+app.post('/api/usuario', function (req, res) {
+    console.log('/api/usuario');
+    console.log(req.body.nome);
+    const lenght = usuarios.filter(usuario => usuario.id === req.body.id).length;
+    console.log(lenght);
+    if (lenght === 0) {
+        usuarios.push(req.body);
+    } else {
+        res.status(400).end();
+    }
+    
+    res.end();
+});
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
