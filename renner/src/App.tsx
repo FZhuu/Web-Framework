b86905f25f85d3ca31c8adc85e3d0d2a7583b685
FZhuu <felipezhangzhu@gmail.com>
2025-04-02 21:23:09 -0300

    Carrosel

--- renner/src/App.tsx
+++ renner/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Menu from './layouts/Menu';
+import Slider from './layouts/slider';
 
 function App() {
   return (
     <div className="container">
       <Menu />
+      <Slider></Slider>
     </div>
   );
 }
